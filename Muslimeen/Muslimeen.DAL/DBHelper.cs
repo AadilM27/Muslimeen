e552ff03052ed383a9896c03a2ad42cfbf2e2105
AHBhat38 <36207194+AHBhat38@users.noreply.github.com>
2018-04-21 15:19:46 +0200

    Added code DBHelper etc
    +DBHelper
    +DBAccess
    +Artilce

---
+++ Muslimeen/Muslimeen.DAL/DBHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace Muslimeen.DAL
+{
+    internal class DBHelper
+    {
+        private static string connString = ConfigurationManager.ConnectionStrings["MuslimeenDB"].ConnectionString;
+
+
+
+    }
+}
