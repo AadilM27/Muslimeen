d6de92cb069850d926a29dca757694ee1d8ca3a7
AHBhat38 <36207194+AHBhat38@users.noreply.github.com>
2018-04-26 21:58:24 +0200

    Design Updates on Login page

---
+++ Muslimeen/Muslimeen/Default.aspx
@@ -0,0 +1,10 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Muslimeen.SignIn.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+        
+</head>
+    
+</html>
--- Muslimeen/Muslimeen/Default.aspx.designer.cs
+++ Muslimeen/Muslimeen/Default.aspx.designer.cs
@@ -11,14 +11,5 @@ namespace Muslimeen.SignIn {
     
     
     public partial class Default {
-        
-        /// <summary>
-        /// btnSignIn control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnSignIn;
     }
 }
