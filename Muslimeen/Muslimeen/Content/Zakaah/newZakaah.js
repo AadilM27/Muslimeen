4b6e18fcec2337da4c84d2de2dcdf028ad07bc9a
UthmaanJappie <38012280+UthmaanJappie@users.noreply.github.com>
2018-06-04 22:14:40 +0200

    zakaah related webforms added
    deleted old zakaah and redoing zakaah

--- Muslimeen/Muslimeen/Content/Zakaah/newZakaah.js
+++ Muslimeen/Muslimeen/Content/Zakaah/newZakaah.js
@@ -1,19 +1,2 @@
 ﻿
-    function openPage(pageName,elmnt,color) {
-        var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-        for (i = 0; i < 1; i++)
-        {
-        tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablink");
-        for (i = 0; i < 1; i++)
-        {
-        tablinks[i].style.backgroundColor = "";
-    }
-    document.getElementById(pageName).style.display = "block";
-    elmnt.style.backgroundColor = color;
 
-}
-// Get the element with id="defaultOpen" and click on it
-    document.getElementById("defaultOpen").click();
