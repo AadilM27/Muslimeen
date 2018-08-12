2661e0251c40c888e614120353dfe807a63de39b
UthmaanJappie <38012280+UthmaanJappie@users.noreply.github.com>
2018-08-12 13:30:45 +0200

    added login verification to moderator

--- Muslimeen/Muslimeen/Content/MyModerator.aspx.cs
+++ Muslimeen/Muslimeen/Content/MyModerator.aspx.cs
@@ -37,17 +37,24 @@ namespace Muslimeen.Content.MyModerator
                 if (Session["UserName"] != null)
                 {
                     uspGetMember uspGetMember = new uspGetMember();
-
                     uspGetMember = dBHandler.BLL_GetMember(Convert.ToString(Session["UserName"]));
-                    hplUserProfile.Text = uspGetMember.MemberLastName + ", " + uspGetMember.MemberName;
-                    divUserProfile.Visible = true;
 
-                    liMyMusbtn.Visible = true;
-                    liMyMusDivi.Visible = true;
+                    if (uspGetMember.MemberType == 'M')
+                    {
+                        hplUserProfile.Text = uspGetMember.MemberLastName + ", " + uspGetMember.MemberName;
+                        divUserProfile.Visible = true;
 
-                    btnLogin.Text = "Log out";
-                    btnRegister.Visible = false;
+                        liMyMusbtn.Visible = true;
+                        liMyMusDivi.Visible = true;
 
+                        btnLogin.Text = "Log out";
+                        btnRegister.Visible = false;
+
+                    }
+                    else
+                    {
+                        Response.Redirect("~/Content/Error.aspx");
+                    }
                 }
                 else if (Session["UserName"] == null)
                 {
@@ -56,6 +63,7 @@ namespace Muslimeen.Content.MyModerator
 
                     divUserProfile.Visible = false;
                     Session.Clear();
+                    Response.Redirect("~/Login/Login.aspx");
                 }
 
 
@@ -137,7 +145,7 @@ namespace Muslimeen.Content.MyModerator
 
                 Session.Clear();
                 Session.Abandon();
-                Response.Redirect("~/Content/MyAdmin.aspx");
+                Response.Redirect("~/Content/MyModerator.aspx");
                 btnLogin.Text = "Login";
                 btnRegister.Visible = true;
             }
@@ -194,17 +202,17 @@ namespace Muslimeen.Content.MyModerator
 
                 uspGetMember = dBHandler.BLL_GetMember(Convert.ToString(Session["UserName"]));
 
-                if (uspGetMember.MemberType == 'A')
+                if (uspGetMember.MemberType == 'M')
+                {
+                    Response.Redirect("~/Content/MyModerator.aspx");
+                }
+                else if (uspGetMember.MemberType == 'A')
                 {
                     Response.Redirect("~/Content/MyAdmin.aspx");
                 }
-                else if (uspGetMember.MemberType == 'M')
-                {
-                    Response.Redirect("~/Content/MyMember.aspx");
-                }
                 else if (uspGetMember.MemberType == 'O')
                 {
-                    Response.Redirect("~/Content/MyModerator.aspx");
+                    Response.Redirect("~/Content/MyMember.aspx");
                 }
                 else if (uspGetMember.MemberType == 'S')
                 {
