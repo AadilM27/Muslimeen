d13725c40c08018149f4a3e2dc4b78a484377940
UthmaanJappie <38012280+UthmaanJappie@users.noreply.github.com>
2018-09-04 15:58:53 +0200

    edited organization page

--- Muslimeen/TypeLib/ViewModels/uspGetOrganizations.cs
+++ Muslimeen/TypeLib/ViewModels/uspGetOrganizations.cs
@@ -8,9 +8,12 @@ namespace TypeLib.ViewModels
 {
     public class uspGetOrganizations
     {
+        
         public string OrgName { get; set; }
         public string Address { get; set; }
         public string OrgImageUrl { get; set; }
         public string ContactNo { get; set; }
+        public string PhysicalAddress { get; set; }
+        public char Active { get; set; }
     }
 }
